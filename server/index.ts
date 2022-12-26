6129886bb81c3b9f2e6fd4d4a5d511f211fc66de
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2022-12-26 14:25:41 +0100

    modified backend connection

--- server/index.ts
+++ server/index.ts
@@ -1,31 +1,41 @@
-import express from "express";
-import cors from "cors";
-import morgan from "morgan";
-import mongoose from "mongoose";
-import userRoutes from "./routes/user";
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import mongoose from 'mongoose';
+import userRoutes from './routes/user';
 
 mongoose.set('strictQuery', false);
 const app = express();
 app.use(cors());
 app.use(morgan('dev'));
-app.use(express.json({
-  limit: '30mb',
-}));
-app.use(express.urlencoded({
-  limit: '30mb',
-  extended: true
-}));
+app.use(
+  express.json({
+    limit: '30mb',
+  })
+);
+app.use(
+  express.urlencoded({
+    limit: '30mb',
+    extended: true,
+  })
+);
 app.use('/', userRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://okoyecharles:A1b2c3d4e5.@cluster0.ymqic93.mongodb.net/test';
+const CONNECTION_URL =
+  'mongodb+srv://techieemma:Nicktrent1@cluster0.vuiwyfj.mongodb.net/?retryWrites=true&w=majority';
 const port = 4000;
 
-mongoose.connect(CONNECTION_URL)
+mongoose
+  .connect(CONNECTION_URL)
   .then(() => {
     app.listen(port, () => {
-      console.log(`Server is running at ${port}`)
-    })
+      console.log(`Server is running at ${port}`);
+    });
   })
   .catch((err) => {
     console.log(err.message);
   });
+
+app.get('/', (req, res) => {
+  res.send('Hello to Memories API');
+});
