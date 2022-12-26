96f7d1f9f6d6cd74e6c1e23a5c1c3e26a252dc65
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2022-12-26 14:25:10 +0100

    add the login and reister

--- client/pages/login.tsx
+++ client/pages/login.tsx
@@ -1,7 +1,48 @@
 import React from 'react';
+import Link from 'next/link';
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <section className="login_container">
+      <div className="login">
+        <h2 className="title">Welcome Back</h2>
+        <form action="#">
+          <div>
+            <label htmlFor="email" className="form_label">
+              Email Address
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              className="form_input"
+              placeholder="your email"
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="password" className="form_label">
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="••••••••"
+              className="form_input"
+              required
+            />
+          </div>
+          <button type="submit" className="btn_submit">
+            Sign in
+          </button>
+          <p className="text">
+            Don’t have an account yet? <Link href="/register">Sign up</Link>
+          </p>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Login;
--- client/pages/register.tsx
+++ client/pages/register.tsx
@@ -1,7 +1,74 @@
 import React from 'react';
+import Link from 'next/link';
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <section className="register_container">
+      <div className="register">
+        <h2 className="title">Sign Up </h2>
+        <form action="#">
+          <div>
+            <label htmlFor="name" className="form_label">
+              First Name
+            </label>
+            <input
+              type="name"
+              name="name"
+              id="first_name"
+              className="form_input"
+              placeholder="enter your name"
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="name" className="form_label">
+              Last Name
+            </label>
+            <input
+              type="name"
+              name="name"
+              id="last_name"
+              className="form_input"
+              placeholder="enter your last name"
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="email" className="form_label">
+              Email Address
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              className="form_input"
+              placeholder="your email"
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="password" className="form_label">
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="••••••••"
+              className="form_input"
+              required
+            />
+          </div>
+          <button type="submit" className="btn_submit">
+            Sign Up
+          </button>
+          <p className="text">
+            Already have an account? <Link href="/login">Sign In</Link>
+          </p>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Register;
